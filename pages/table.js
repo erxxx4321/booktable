a58769fac71bd2c2607d7b7c9f099ecb9c79279f
Yu Fang Chin <>
2021-07-26 20:58:52 +0800

    adjust time format

--- pages/table.js
+++ pages/table.js
@@ -3,13 +3,21 @@ var router = express.Router();
 var connection = require("../DBconnection");
 
 router.get("/", function (req, res, next) {
-	connection.query("select * from Reservation", (err, rows, field) => {
-		if (err) throw err;
-		res.render("table", {
-			title: "PICO Reservation",
-			items: rows,
-		});
-	});
+	connection.query(
+		"SELECT DATE_FORMAT(Time,GET_FORMAT(TIME,'JIS')) as Time,  Person, Name, Phone FROM sp.Reservation;",
+		(err, rows, field) => {
+			if (err) throw err;
+
+			const data = rows.map((item) => {
+				item.Time = item.Time.slice(0, -3);
+				return item;
+			});
+			res.render("table", {
+				title: "PICO Reservation",
+				items: data,
+			});
+		}
+	);
 });
 
 module.exports = router;
