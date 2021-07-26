0ae7204f86967bb3858360fd462cb962138d8c04
Yu Fang Chin <>
2021-07-26 11:54:07 +0800

    development configuration

--- DBconnection.js
+++ DBconnection.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "1qaz@WSX",
-	database: "sp",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_DATABASE,
 });
 
 connection.connect((err) => {
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var cors = require("cors");
 var app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 var indexPage = require("./pages/index");
 var tablePage = require("./pages/table");
@@ -18,6 +20,8 @@ app.use(express.static(path.join(__dirname, "static")));
 app.use("/", indexPage);
 app.use("/table", tablePage);
 
-app.listen(4000, () => {
-	console.log("Start server on port 4000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Start server on port ${port}`);
 });
